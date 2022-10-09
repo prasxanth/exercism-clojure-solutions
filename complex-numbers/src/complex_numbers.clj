a737c014b0d04ab1b73e0ac43222b4a614d4f973
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-09 15:16:09 -0700

    Reformatted complex-numbers.

--- complex-numbers/src/complex_numbers.clj
+++ complex-numbers/src/complex_numbers.clj
@@ -19,10 +19,10 @@
 (def add (partial map +))
 
 (def sub (partial map -))
- 
+
 (def mul (partial proc - +))
 
-(defn div [[a b] [c d]] ;; <- arglist goes here
+(defn div [[a b] [c d]]
   (let [denom (sum-of-squares c d)]
     (->> (proc + - [a b] [c d])
-      (mapv #(/ % denom 1.0)))))
+         (mapv #(/ % denom 1.0)))))
