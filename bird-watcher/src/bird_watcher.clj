33f86183b9565212b9b11f413402c6c14f0081b6
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-09 15:00:09 -0700

    Reformatted bird-watcher.

--- bird-watcher/src/bird_watcher.clj
+++ bird-watcher/src/bird_watcher.clj
@@ -1,31 +1,22 @@
 (ns bird-watcher)
 
-(def last-week 
+(def last-week
   [0 2 5 3 7 8 4])
 
 (defn today [birds]
   (last birds))
 
 (defn inc-bird [birds]
-  (->> birds 
-    last 
-    inc 
-    (conj (pop birds))))
+  (->> birds last inc (conj (pop birds))))
 
 (defn day-without-birds? [birds]
-   (->> birds
-     (some zero?) 
-     boolean))
+  (->> birds (some zero?) boolean))
 
 (defn n-days-count [birds n]
-  (->> birds
-    (take n)
-    (reduce +)))
+  (->> birds (take n) (reduce +)))
 
 (defn busy-days [birds]
-  (->> birds
-    (filter #(>= % 5)) 
-    count))
+  (->> birds (filter #(>= % 5)) count))
 
 (defn odd-week? [birds]
   (= birds [1 0 1 0 1 0 1]))
