a5b48e5d51de4eea39fae853913a1d1833739105
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-09 14:53:31 -0700

    Reformatted anagram.

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,15 +1,10 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(require '[clojure.string :as str])
-
-(defn anagram?
- "Returns true if candidate is an anagram of word"
- [word candidate]
+(defn anagram? [word candidate]
   (let [x (str/lower-case word)
         y (str/lower-case candidate)]
     (and (not= x y) (= (sort x) (sort y)))))
 
-(defn anagrams-for 
- "Given a word and a list of candidates, select the sublist of anagrams of the given word"
- [word candidates]
- (filter (partial anagram? word) candidates))
+(defn anagrams-for [word candidates]
+  (filter (partial anagram? word) candidates))
