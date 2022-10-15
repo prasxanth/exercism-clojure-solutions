52fe89d981e0461ae65b0bcc80f38ce533acda8f
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-15 15:38:16 -0700

    Updated elyses-destructured-enchantments.

--- elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
+++ elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
@@ -1,29 +1,29 @@
-(ns elyses-destructured-enchantments)
+ (ns elyses-destructured-enchantments)
 
 (defn first-card
   "Returns the first card from deck."
   [deck]
-  (let [[first-card%] deck]
-  first-card%))
+  (let [[unua-karto] deck]
+    unua-karto))
 
 (defn second-card
   "Returns the second card from deck."
   [deck]
-    (let [[_ second-card%] deck]
-  second-card%))
+  (let [[_ dua-karto] deck]
+    dua-karto))
 
 (defn swap-top-two-cards
   "Returns the deck with first two items reversed."
   [deck]
-  (let [[first-card% second-card% & remaining-cards% :as all] deck]
-  (conj remaining-cards% first-card% second-card%)))
+  (let [[first-card second-card & remaining-cards] deck]
+    (conj remaining-cards first-card second-card)))
 
 (defn discard-top-card
   "Returns a vector containing the first card and
    a vector of the remaining cards in the deck."
   [deck]
-    (let [[first-card% & remaining-cards% :as all] deck]
-  [first-card% remaining-cards%]))
+  (let [[top-card & remaining-cards] deck]
+    [top-card remaining-cards]))
 
 (def face-cards
   ["jack" "queen" "king"])
@@ -31,8 +31,5 @@
 (defn insert-face-cards
   "Returns the deck with face cards between its head and tail."
   [deck]
-  (let [[first-card% & remaining-cards% :as all] deck]
-   (->> (conj remaining-cards% face-cards first-card%)
-    flatten
-    vec
-    (remove nil?))))
+  (let [[first-card & remaining-cards] deck]
+    (->> (flatten [first-card face-cards remaining-cards]) (remove nil?))))
