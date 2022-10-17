5d17a60af7229292062bdbe368776ac095f1fd9b
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-17 00:30:01 -0700

    Updated nth prime.

--- nth-prime/src/nth_prime.clj
+++ nth-prime/src/nth_prime.clj
@@ -1,11 +1,15 @@
 (ns nth-prime)
 
-(defn divides? [m n] (zero? (mod m n)))
-
-(defn prime? [n]
- (->> (range 2 n)
-  (not-any? #(divides? n %))))
+;; https://stackoverflow.com/questions/960980/fast-prime-number-generation-in-clojure
+;; See https://otee.dev/2022/01/17/lazy-clojure.html for an excellent explanation!
+(def primes
+  (cons 1 (lazy-seq
+            (filter (fn [i]
+                      (not-any? (fn [p] (zero? (rem i p)))
+                                (take-while #(<= % (Math/sqrt i)) (rest primes))))
+                    (drop 2 (range))))))
 
 (defn nth-prime [n]
-  (if (zero? n) (throw (IllegalArgumentException. "There is no zeroth prime"))
-    (nth (->> (range 2 (* 10 n)) (filter prime?)) (dec n))))
+  (if (zero? n)
+    (throw (IllegalArgumentException. "There is no zeroth prime"))
+    (nth primes n)))
