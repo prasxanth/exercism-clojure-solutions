993c33ddd6f730ff538f6bb11f8ab63edfd26e3d
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-17 23:25:26 -0700

    Reformatted space-age.

--- space-age/src/space_age.clj
+++ space-age/src/space_age.clj
@@ -1,17 +1,15 @@
 (ns space-age)
 
-(def planet-years {"mercury" 0.2408467 "venus" 0.61519726 "earth" 1.0 "mars" 1.8808158 "jupiter" 11.862615
-                   "saturn" 29.447498  "uranus" 84.016846 "neptune" 164.79132})
+(def planet-years {"mercury" 0.2408467 "venus" 0.61519726  "earth" 1.0
+                   "mars" 1.8808158    "jupiter" 11.862615 "saturn" 29.447498
+                   "uranus" 84.016846  "neptune" 164.79132})
 
 (def ^:const earth-year-in-seconds 31557600)
 
 (defmacro on-planets [years arg]
-  (let [planetarum# (eval years)]
-   `(do ~@(for [p#  (keys planetarum#)]
-            `(defn ~(symbol (apply str "on-" p#)) [~arg]
-               (->> ~p# ~planetarum# (* earth-year-in-seconds) (/ ~arg)))))))
-
-#_(macroexpand '(on-planets planet-years age))
-#_(on-earth 2134835688)
+  (let [planet-years# (eval years)]
+    `(do ~@(for [p# (keys planet-years#)]
+             `(defn ~(symbol (apply str "on-" p#)) [~arg]
+                (->> ~p# ~planet-years# (* earth-year-in-seconds) (/ ~arg)))))))
 
 (on-planets planet-years age)
