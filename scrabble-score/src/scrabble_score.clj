37558d3b81617492cb7b82073581a819d0b8aee0
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-17 23:18:38 -0700

    Reformatted scrabble-score.

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -1,7 +1,6 @@
 (ns scrabble-score
   (:require [clojure.string :as str]))
 
-
 (def scores {\A 1 \E 1 \I 1 \O 1 \U 1 \L 1 \N 1 \R 1 \S 1 \T 1
              \D 2 \G 2
              \B 3 \C 3 \M 3 \P 3
