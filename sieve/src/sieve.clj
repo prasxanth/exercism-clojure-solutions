1c2a4538825e5551812370e119953876b3fb9b57
Prashanth Kumar <prasxanth.kumar@gmail.com>
2022-10-17 23:20:30 -0700

    Reformatted sieve.

--- sieve/src/sieve.clj
+++ sieve/src/sieve.clj
@@ -5,5 +5,6 @@
 
 (defn sieve [limit]
   (loop [[x & xs] (irange 3 limit 2) primes [2]]
-    (if (= 1 (count xs)) (concat primes [x] xs)
-        (recur (remove (into #{} (irange x (last xs) x)) xs) (conj primes x)))))
+    (if (= 1 (count xs))
+      (concat primes [x] xs)
+      (recur (remove (into #{} (irange x (last xs) x)) xs) (conj primes x)))))
